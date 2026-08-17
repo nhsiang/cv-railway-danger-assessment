07b67db5acd6e33f3419e8438b343e0b5056378e
nhsiang <nhsiang4912@hotmail.com>
2026-08-17 11:34:13 -0500

    readme

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from ultralytics import YOLO
 
-# model_detection = YOLO("models/yolo26n.pt")
+model_detection = YOLO("models/yolo26n.pt")
 model_segmentation = YOLO("models/runs/semantic/train/weights/best.pt")
 video = cv.VideoCapture('./assets/video/kibukawa_to_minakuchijyonan.mp4')
 
@@ -10,7 +10,8 @@ while True:
     isTrue, frame = video.read()
     frame = frame[540:1060,580:1450]
 
-    results_segmentation = model_segmentation(frame)
+    results_detection, results_segmentation = model_detection(frame), model_segmentation(frame)
+    # boxes = results_detection[0]
     masks = results_segmentation[0].semantic_mask.data.cpu().numpy()
 
     # for i in [1, 2, 3]: # verifying masks
@@ -18,7 +19,7 @@ while True:
     #     cv.imshow(f"class {i}", mask)
 
     track_mask = (masks == 1).astype(np.uint8)
-    
+
     # plot railway path (based on mask rail-track)
     coords = []
     for y in range(track_mask.shape[0] - 1, 0, -10):
@@ -29,7 +30,7 @@ while True:
             mid_x = (left + right) // 2
             coords.append((mid_x, y))
     coords = np.array(coords)
-    
+
     # calculate curvature
     f = np.polyfit(coords[:, 1], coords[:, 0], 2)  # x=f(y)=ay^2+by+c
     f_p, f_pp = np.polyder(f, 1), np.polyder(f, 2)
@@ -37,12 +38,23 @@ while True:
     dx, ddx = np.polyval(f_p, y), np.polyval(f_pp, y)
     k = np.abs(ddx) / (1 + dx ** 2) ** 1.5  # κ(y)=|f''(y)|/(1+[f'(y)]^2)^(3/2)
     k_frame = np.median(k)
-    
+
+    # verdict
+    curve_threshold = 0.001
+    if k_frame > curve_threshold:
+        status = "CURVE AHEAD"
+    else:
+        status = "NORMAL"
+
     # output
     screen = frame.copy()
-    cv.putText(screen, f'median curvature: {k_frame}', (50, 50), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255), 2)
-    
-    cv.imshow("YOLO", track_mask)
+    cv.putText(screen, f'Relative curvature: {k_frame}', (50, 50), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255), 2)
+    cv.putText(screen, status, (50, 100), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255, 255, 255), 2)
+
+    # ys = np.arange(coords[:, 1].min(), coords[:, 1].max())
+    # pts = np.array([(np.polyval(f, y), y) for y in ys], dtype=np.int32)
+    # cv.polylines(screen, [pts], False, (255, 0, 0), 2)
+
     cv.imshow("output", screen)
     # cv.imshow("output", results_segmentation[0].plot())
 
